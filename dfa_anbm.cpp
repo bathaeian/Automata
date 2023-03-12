dd55c6a43537ca811205d7de0da248b5dbedc59c
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2023-03-12 08:48:37 +0330

    Update dfa_anbm.cpp

--- dfa_anbm.cpp
+++ dfa_anbm.cpp
@@ -1,7 +1,9 @@
+//DFA L={a*bb*}
 #include<iostream>
 #include<string>
 bool anbm(char* w , int state){
     if(*w==0 && state==1) return true;
+    if(*w==0 && state==0) return false;
     if(*w=='a' && state==1) return false;
     if(*w=='b' && state==0)return anbm(++w,1);
     return anbm(++w,state);
