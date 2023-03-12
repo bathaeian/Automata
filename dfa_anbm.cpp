e5ff205f1a1918a8dd7f07b51c5ffe0d42645846
Narges S. Bathaeian <narges.s.bathaeian@gmail.com>
2023-03-12 08:00:21 +0330

    Add files via upload
    DFA for L= {a*bb*}

---
+++ dfa_anbm.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+bool anbm(char* w , int state){
+    if(*w==0 && state==1) return true;
+    if(*w=='a' && state==1) return false;
+    if(*w=='b' && state==0)return anbm(++w,1);
+    return anbm(++w,state);
+}
+int main(){
+    char* s;
+    s=new char[80];
+    std::cin>>s;
+    std::cout<<anbm(s,0);
+    return 0;
+}
